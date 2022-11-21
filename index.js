07c1b697bb607fca62c5eeae8819b0aaee24a366
Md.Ashraful Islam Khan <19103189@iubat.edu>
2022-11-21 06:40:04 +0600

    add-doc

--- index.js
+++ index.js
@@ -123,9 +123,18 @@ async function run() {
       const result = await usersCollection.find(query).toArray()
       res.send(result)
     })
+
+    app.get('/users/admin/:email', async (req, res) => {
+      const email = req.params.email
+      const query = { email }
+      const user = await usersCollection.findOne(query)
+      console.log(user);
+      res.send({ isAdmin: user?.role === 'admin' })
+    })
+
     app.put('/users/admin/:id', verifyJWT, async (req, res) => {
-      const decodedEmail = req.decoded.email;
-      const query = { email: decodedEmail };
+      const decodedEmail = req.decoded.email
+      const query = { email: decodedEmail }
       const user = await usersCollection.findOne(query)
       if (user?.role !== 'admin') {
         return res.status(403).send('forbidden access')
@@ -135,10 +144,14 @@ async function run() {
       const options = { upsert: true }
       const updatedDoc = {
         $set: {
-          role:'admin'
-        }
+          role: 'admin',
+        },
       }
-      const result = await usersCollection.updateOne(filter, updatedDoc, options)
+      const result = await usersCollection.updateOne(
+        filter,
+        updatedDoc,
+        options
+      )
       res.send(result)
     })
   } finally {
